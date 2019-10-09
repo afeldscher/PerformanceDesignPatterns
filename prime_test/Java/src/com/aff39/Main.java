862006dab1cd9c72b281e99fe5ae03f79c737536
Adam Feldscher <aff39@drexel.edu>
2019-10-09 16:13:03 -0400

    -m

--- prime_test/Java/src/com/aff39/Main.java
+++ prime_test/Java/src/com/aff39/Main.java
@@ -12,35 +12,55 @@ package com.aff39;
 // run times using the same algorithm so it will not matter.
 
 // TODO:
-// timing
-// pauses
-// check
-// bigger numbers
 // C!!!!
+
+import java.io.IOException;
 
 import static java.lang.Math.sqrt;
 
 public class Main {
 
     public static void main(String[] args) {
+        int max_prime = 5000000; // Correct up through 1013
+        int num_primes_found = 0;
+        int max_iter = 20;
 
-        int max_prime = 22;
-        for (int n = 2; n <= max_prime; n++) {
-            //This will get floored. Its ok, because if the integer above the sqrt is a divisor,
-            // the other integer divisor would have had to have been below the sqrt
-            int max_divisor = (int) sqrt(n);
-            boolean is_prime = true;
+        System.out.println("iter, ns, sec, primes found");
 
-            for (int i = 2; i <= max_divisor; i++) {
-                if (n % i == 0) {
-                    is_prime = false;
-                    break;
+        for (int iter = 0; iter < max_iter; iter++) {
+//            try {
+//                Thread.sleep(100);
+//                System.in.read();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            long start_time = System.nanoTime();
+
+            for (int n = 2; n <= max_prime; n++) {
+                //This will get floored. Its ok, because if the integer above the sqrt is a divisor,
+                // the other integer divisor would have had to have been below the sqrt
+                int max_divisor = (int) sqrt(n);
+                boolean is_prime = true;
+
+                for (int i = 2; i <= max_divisor; i++) {
+                    if (n % i == 0) {
+                        is_prime = false;
+                        break;
+                    }
+                }
+
+                if (is_prime) {
+                    // System.out.println(n);
+                    num_primes_found++;
                 }
             }
+            long end_time = System.nanoTime();
+            System.out.println(iter + ", " + (end_time - start_time) + ", " + (end_time - start_time) / 1e9 + ", " + num_primes_found);
 
-            if (is_prime) {
-                System.out.println(n);
-            }
+            num_primes_found = 0;
         }
+
     }
 }
