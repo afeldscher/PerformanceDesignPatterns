4c9c9272fdca229207e7775d04fe9e521d61935e
Adam Feldscher <aff39@drexel.edu>
2019-10-16 15:28:58 -0400

    Adding primefinder + is_prime

--- prime_test/C++/primefinder.cpp
+++ prime_test/C++/primefinder.cpp
@@ -7,22 +7,22 @@ PrimeFinder::PrimeFinder(int max_prime) {
     _max_prime = max_prime;
 }
 
+bool PrimeFinder::is_prime(int n) {
+    int max_divisor = (int) sqrt(n);
+    for (int i = 2; i <= max_divisor; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
 int PrimeFinder::find_primes() {
     int num_primes_found = 0;
     int n;
     for (n = 2; n <= _max_prime; n++) {
-        // int max_divisor = (int) sqrt(n);
-        bool is_prime = true;
-
-        int i;
-        for (i = 2; i <= (int) sqrt(n); i++) {
-            if (n % i == 0) {
-                is_prime = false;
-                break;
-            }
-        }
-
-        if (is_prime) {
+        if (is_prime(n)) {
             num_primes_found++;
         }
     }
--- prime_test/C++/primefinder.h
+++ prime_test/C++/primefinder.h
@@ -4,6 +4,7 @@ class PrimeFinder
 {
 private:
     int _max_prime;
+    bool is_prime(int n);
 
 public:
     PrimeFinder(int max_prime);
