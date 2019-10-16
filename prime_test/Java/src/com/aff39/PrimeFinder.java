6edc299669515ed8c55c816e8226c8b47cb86cb1
Adam Feldscher <aff39@drexel.edu>
2019-10-16 13:33:33 -0400

    Adding primes SQRT

--- prime_test/Java/src/com/aff39/PrimeFinder.java
+++ prime_test/Java/src/com/aff39/PrimeFinder.java
@@ -17,10 +17,10 @@ public class PrimeFinder {
         for (Integer n = 2; n <= max_prime; n++) {
             //This will get floored. Its ok, because if the integer above the sqrt is a divisor,
             // the other integer divisor would have had to have been below the sqrt
-            Integer max_divisor = (int) sqrt(n);
+//            Integer max_divisor = (int) sqrt(n);
             Boolean is_prime = true;
 
-            for (Integer i = 2; i <= max_divisor; i++) {
+            for (Integer i = 2; i <= (int) sqrt(n); i++) {
                 if (n % i == 0) {
                     is_prime = false;
                     break;
