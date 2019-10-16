5291f3ff446bc57502402af9f375b497d5703a49
Adam Feldscher <aff39@drexel.edu>
2019-10-16 15:11:22 -0400

    Adding C++ PrimeFinder

--- prime_test/C++/main.cpp
+++ prime_test/C++/main.cpp
@@ -11,10 +11,11 @@
 #include <time.h>
 #include <stdbool.h>
 #include <math.h>
+#include "primefinder.h"
 
 
 int main(int argc, char *argv[]) {
-    int max_prime = 10000; // Correct up through 1013
+        int max_prime = 10000; // Correct up through 1013
         int num_primes_found = 0;
         int max_iter = 40;
 
@@ -24,25 +25,9 @@ int main(int argc, char *argv[]) {
             struct timespec tstart={0,0}, tend={0,0};
             clock_gettime(CLOCK_MONOTONIC, &tstart);
 
-            int n;
-            for (n = 2; n <= max_prime; n++) {
-                //This will get floored. Its ok, because if the integer above the sqrt is a divisor,
-                // the other integer divisor would have had to have been below the sqrt
-                int max_divisor = (int) sqrt(n);
-                bool is_prime = true;
-
-                int i;
-                for (i = 2; i <= max_divisor; i++) {
-                    if (n % i == 0) {
-                        is_prime = false;
-                        break;
-                    }
-                }
-
-                if (is_prime) {
-                    num_primes_found++;
-                }
-            }
+            PrimeFinder primefinder = PrimeFinder(max_prime);
+            num_primes_found = primefinder.find_primes();
+          
             clock_gettime(CLOCK_MONOTONIC, &tend);
 
             long nano_sec = (tend.tv_nsec + tend.tv_sec * 1e9) - (tstart.tv_nsec + tstart.tv_sec * 1e9);
