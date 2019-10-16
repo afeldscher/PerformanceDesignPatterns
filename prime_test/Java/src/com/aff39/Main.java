708ae7ae8c79a9ef4760d4a2d007d06752b664ba
Adam Feldscher <aff39@drexel.edu>
2019-10-16 12:32:48 -0400

    -m

--- prime_test/Java/src/com/aff39/Main.java
+++ prime_test/Java/src/com/aff39/Main.java
@@ -11,40 +11,30 @@ package com.aff39;
 // This is NOT an optimal implementation for finding primes, however we are trying to compare
 // run times using the same algorithm so it will not matter.
 
-// TODO:
-// C!!!!
 
-import java.io.IOException;
 
 import static java.lang.Math.sqrt;
 
 public class Main {
 
     public static void main(String[] args) {
-        int max_prime = 5000000; // Correct up through 1013
-        int num_primes_found = 0;
-        int max_iter = 20;
+        Integer max_prime = 10000; // Correct up through 1013
+        Integer num_primes_found = 0;
+        Integer max_iter = 40;
 
         System.out.println("iter, ns, sec, primes found");
 
-        for (int iter = 0; iter < max_iter; iter++) {
-//            try {
-//                Thread.sleep(100);
-//                System.in.read();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+        for (Integer iter = 0; iter < max_iter; iter++) {
+
             long start_time = System.nanoTime();
 
-            for (int n = 2; n <= max_prime; n++) {
+            for (Integer n = 2; n <= max_prime; n++) {
                 //This will get floored. Its ok, because if the integer above the sqrt is a divisor,
                 // the other integer divisor would have had to have been below the sqrt
-                int max_divisor = (int) sqrt(n);
-                boolean is_prime = true;
+                Integer max_divisor = (int) sqrt(n);
+                Boolean is_prime = true;
 
-                for (int i = 2; i <= max_divisor; i++) {
+                for (Integer i = 2; i <= max_divisor; i++) {
                     if (n % i == 0) {
                         is_prime = false;
                         break;
@@ -52,11 +42,10 @@ public class Main {
                 }
 
                 if (is_prime) {
-                    // System.out.println(n);
                     num_primes_found++;
                 }
             }
-            long end_time = System.nanoTime();
+            Long end_time = System.nanoTime();
             System.out.println(iter + ", " + (end_time - start_time) + ", " + (end_time - start_time) / 1e9 + ", " + num_primes_found);
 
             num_primes_found = 0;
